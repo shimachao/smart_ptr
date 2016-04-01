39a568694695c6428ee6ee6d7ceccc0d19b6f2ac
超锅锅 <531236305@qq.com>
2016-04-01 23:44:39 +0800

    初步决定用引用计数管理对象。

--- smart_ptr/smart_ptr.h
+++ smart_ptr/smart_ptr.h
@@ -1,3 +1,24 @@
 #pragma once
+#include <map>
 
 // 自己模仿shared_ptr实现一个智能指针
+template <typename T>
+class smart_ptr
+{
+public:
+	smart_ptr();
+	~smart_ptr();
+
+private:
+	unsigned m_use_count = 0;
+};
+
+template <typename T>
+smart_ptr::smart_ptr()
+{
+}
+
+template <typename T>
+smart_ptr::~smart_ptr()
+{
+}
