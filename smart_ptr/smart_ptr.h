155264779939d8a708f0095acdfcf85fd0b3c054
超锅锅 <531236305@qq.com>
2016-04-02 10:31:33 +0800

    添加拷贝构造函数。

--- smart_ptr/smart_ptr.h
+++ smart_ptr/smart_ptr.h
@@ -8,6 +8,8 @@ class smart_ptr
 public:
 	smart_ptr();
 	explicit smart_ptr(T*);
+	smart_ptr(const smart_ptr&);
+
 	~smart_ptr();
 
 	T* get();
@@ -28,6 +30,27 @@ template <typename T>
 smart_ptr::smart_ptr(T*p)
 	:m_pobject(p), m_use_count(new unsigned(1))
 {
+}
+
+
+template <typename T>
+smart_ptr::smart_ptr(const smart_ptr& rhs)
+{
+	// 递减左侧计数
+	if (*m_use_count > 0)
+	{
+		*m_use_count--;
+	}
+	if (*m_use_count == 0)
+	{
+		delete m_pobject;
+	}
+
+	// 递增右侧计数
+	*(rhs.m_use_count)++;
+
+	m_pobject = rhs.m_pobject;
+	m_use_count = rhs.m_use_count;
 }
 
 
